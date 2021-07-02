e44cf222dc50c9ba41562ddb785d8e8f7ccd02af
alexo <alex-ong>
2021-07-03 04:05:58 +1000

    new: AI that re-generates file if the output file is the wrong size

--- discord-vid.bat
+++ discord-vid.bat
@@ -2,4 +2,3 @@
 echo %~dp0
 SET output=%~n1_8MB.mp4
 python %~dp0disvid.py -i %1 -c:v libx264 -vf scale=1280:-1 -r 30 %output%
-pause
--- disvid.py
+++ disvid.py
@@ -3,6 +3,7 @@ import subprocess
 import glob
 import os
 
+TARGET_SIZE = 8100
 def get_length(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
                              "format=duration", "-of",
@@ -31,6 +32,35 @@ def delete_logs():
     for file_path in file_list:    
         os.remove(file_path)
     
+def generate_file(target_size):
+    bitrate = (target_size*8 - audio_rate*length)/ length
+    if bitrate < 0:
+        print(f"Unfortunately there is not enough bits for video!")
+        sys.exit()
+    
+    command = (["ffmpeg", "-y"] + sys.argv[1:-1] + 
+              ["-threads", "8", "-speed", "4", "-row-mt", "1", "-tile-columns", "2",
+              "-vsync", "cfr",
+              "-b:v", f"{bitrate:.0f}k", "-minrate", f"{bitrate/2:.0f}k",
+              "-maxrate", f"{bitrate*1.5:.0f}k", "-an",
+              "-pass", "1", "-f", "mp4","NUL"])
+              
+    print (" ".join(command))
+    result = subprocess.call(command)
+    
+    command = (["ffmpeg", "-y"] + sys.argv[1:-1] + 
+              ["-b:v", f"{bitrate:.0f}k", "-minrate", f"{bitrate/2:.0f}k",
+              "-threads", "8", "-speed", "2", "-row-mt", "1", "-tile-columns", "2",
+              "-maxrate", f"{bitrate*1.5:.0f}k",
+              "-b:a", "64k",              
+              "-pass", "2", sys.argv[-1], "-y"])
+              
+    
+    print (" ".join(command))
+    subprocess.call(command)
+       
+    delete_logs()
+    return os.path.getsize(sys.argv[-1])
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -49,30 +79,11 @@ if __name__ == "__main__":
     audio_rate = 64
     print(f"estimated audio size:, {audio_rate*length/8}KB")
     
-    bitrate = (8100*8 - audio_rate*length)/ length
-    if bitrate < 0:
-        print(f"Unfortunately there is not enough bits for video!")
-        sys.exit()
-        
-    command = (["ffmpeg", "-y"] + sys.argv[1:-1] + 
-              ["-threads", "8", "-speed", "4", "-row-mt", "1", "-tile-columns", "2",
-              "-vsync", "cfr",
-              "-b:v", f"{bitrate:.0f}k", "-minrate", f"{bitrate/2:.0f}k",
-              "-maxrate", f"{bitrate*1.5:.0f}k", "-an",
-              "-pass", "1", "-f", "mp4","NUL"])
-              
-    print (" ".join(command))
-    result = subprocess.call(command)
+    target_size = TARGET_SIZE
+    actual_size = generate_file(target_size)
     
-    command = (["ffmpeg", "-y"] + sys.argv[1:-1] + 
-              ["-b:v", f"{bitrate:.0f}k", "-minrate", f"{bitrate/2:.0f}k",
-              "-threads", "8", "-speed", "2", "-row-mt", "1", "-tile-columns", "2",
-              "-maxrate", f"{bitrate*1.5:.0f}k",
-              "-b:a", "64k",              
-              "-pass", "2", sys.argv[-1], "-y"])
-              
-    print (" ".join(command))
-    subprocess.call(command)
-       
-    delete_logs()
+    while actual_size > 8*1024*1024:
+        print("Uh oh, we're still over size: ", actual_size/1024/1024.0)
+        target_size -= 100
+        actual_size = generate_file(target_size)
     
