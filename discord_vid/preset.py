5b99c689e959d73a797266ad6c1c3eef3e20e761
alexo <alex-ong>
2022-08-07 11:58:33 +1000

    new: Move install_context to not require a sample file

--- discord_vid/preset.py
+++ discord_vid/preset.py
@@ -3,21 +3,22 @@ Presets for users
 """
 
 from discord_vid.config import get_config
-from enum import Enum
 
-MB_TO_BYTES = 1024*1024
+MB_TO_BYTES = 1024 * 1024
 
 
 def get_size(min_size, max_size):
     """Converts min/max tuple to min/target/max triplet"""
     return [min_size, 0, max_size]
 
+
 def presetdata_to_options(data):
     """Convert json dictionary to tuple of size and options"""
     min_size = int(data["min_size_mb"] * MB_TO_BYTES)
     max_size = int(data["max_size_mb"] * MB_TO_BYTES)
     options = data["args"]
-    return (get_size(min_size,max_size), options)
+    return (get_size(min_size, max_size), options)
+
 
 def get_preset_options(preset: str):
     """
@@ -30,10 +31,17 @@ def get_preset_options(preset: str):
     if preset not in presets:
         raise ValueError("Invalid preset: {preset}")
     return presetdata_to_options(presets[preset])
-        
-def display_presets():
+
+def get_presets():
+    """ Return list of preset names """
     config = get_config()
-    print ("Preset list:\n" + 
-           "\n".join(config["presets"].keys()))
+    return config["presets"].keys()
+
+def display_presets():
+    """ Prints out preset names """
+    config = get_config()
+    presets = config["presets"].keys()
+    print("Preset list:\n" + "\n".join(presets))
     default_preset = config["default_preset"]
-    print (f"Default preset: {default_preset}")
+    print(f"Default preset: {default_preset}")
+    return config["presets"].keys()
