cc079021d866e053f2e0fbb57adcae9d613b2844
alexo <the.onga@gmail.com>
2026-08-22 00:28:10 +1000

    Add automatic zip maker

--- build.bat
+++ build.bat
@@ -2,6 +2,15 @@
 uv sync
 
 set PYINSTALLER_ARGS=--noconfirm
-if /i "%~1"=="clean" set PYINSTALLER_ARGS=%PYINSTALLER_ARGS% --clean
+set DO_ZIP=0
+
+for %%A in (%*) do (
+    if /i "%%A"=="clean" set PYINSTALLER_ARGS=%PYINSTALLER_ARGS% --clean
+    if /i "%%A"=="zip" set DO_ZIP=1
+)
 
 uv run pyinstaller main.spec %PYINSTALLER_ARGS%
+
+if %DO_ZIP%==1 (
+    pwsh -Command "Compress-Archive -Path dist -DestinationPath discord_vid-$((Get-Date).ToString('yyyyMMdd')).zip -Force"
+)
